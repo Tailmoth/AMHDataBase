efe26ee2f7468417010d8ff136b6862967a1bf4f
Tailmoth <74428564+Tailmoth@users.noreply.github.com>
2020-11-21 12:26:52 -0600

    Add files via upload

---
+++ AMHDataBase/AMHDataBase/EditData.Designer.cs
@@ -0,0 +1,293 @@
+﻿namespace AeroMaterialHandlingDatabaseApplication
+{
+    partial class fEditPage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(fEditPage));
+            this.tbEditTittle = new System.Windows.Forms.TextBox();
+            this.tbEditShortDesc = new System.Windows.Forms.TextBox();
+            this.btEditSave = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbEditLongDesc = new System.Windows.Forms.TextBox();
+            this.btEditClear = new System.Windows.Forms.Button();
+            this.btEditExit = new System.Windows.Forms.Button();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label16 = new System.Windows.Forms.Label();
+            this.tbEditAddTags = new System.Windows.Forms.TextBox();
+            this.btTagAdd = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btEditRemove = new System.Windows.Forms.Button();
+            this.btEditAdd = new System.Windows.Forms.Button();
+            this.btEditDeleteTag = new System.Windows.Forms.Button();
+            this.btEditAddImage = new System.Windows.Forms.Button();
+            this.pbRegister = new System.Windows.Forms.PictureBox();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbRegister)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tbEditTittle
+            // 
+            this.tbEditTittle.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbEditTittle.Location = new System.Drawing.Point(277, 23);
+            this.tbEditTittle.Name = "tbEditTittle";
+            this.tbEditTittle.Size = new System.Drawing.Size(511, 38);
+            this.tbEditTittle.TabIndex = 1;
+            this.tbEditTittle.TextChanged += new System.EventHandler(this.tbEditTittle_TextChanged);
+            // 
+            // tbEditShortDesc
+            // 
+            this.tbEditShortDesc.Location = new System.Drawing.Point(219, 285);
+            this.tbEditShortDesc.Multiline = true;
+            this.tbEditShortDesc.Name = "tbEditShortDesc";
+            this.tbEditShortDesc.Size = new System.Drawing.Size(447, 70);
+            this.tbEditShortDesc.TabIndex = 5;
+            // 
+            // btEditSave
+            // 
+            this.btEditSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btEditSave.Location = new System.Drawing.Point(878, 440);
+            this.btEditSave.Name = "btEditSave";
+            this.btEditSave.Size = new System.Drawing.Size(71, 40);
+            this.btEditSave.TabIndex = 9;
+            this.btEditSave.Text = "Save";
+            this.btEditSave.UseVisualStyleBackColor = true;
+            this.btEditSave.Click += new System.EventHandler(this.btSave_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(197, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 31);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Title:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(197, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 25);
+            this.label2.TabIndex = 20;
+            this.label2.Text = "Tags:";
+            // 
+            // tbEditLongDesc
+            // 
+            this.tbEditLongDesc.Location = new System.Drawing.Point(219, 365);
+            this.tbEditLongDesc.Multiline = true;
+            this.tbEditLongDesc.Name = "tbEditLongDesc";
+            this.tbEditLongDesc.Size = new System.Drawing.Size(447, 115);
+            this.tbEditLongDesc.TabIndex = 36;
+            // 
+            // btEditClear
+            // 
+            this.btEditClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btEditClear.Location = new System.Drawing.Point(955, 440);
+            this.btEditClear.Name = "btEditClear";
+            this.btEditClear.Size = new System.Drawing.Size(71, 40);
+            this.btEditClear.TabIndex = 37;
+            this.btEditClear.Text = "Clear";
+            this.btEditClear.UseVisualStyleBackColor = true;
+            this.btEditClear.Click += new System.EventHandler(this.btClear_Click);
+            // 
+            // btEditExit
+            // 
+            this.btEditExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btEditExit.Location = new System.Drawing.Point(1032, 440);
+            this.btEditExit.Name = "btEditExit";
+            this.btEditExit.Size = new System.Drawing.Size(71, 40);
+            this.btEditExit.TabIndex = 38;
+            this.btEditExit.Text = "Exit";
+            this.btEditExit.UseVisualStyleBackColor = true;
+            this.btEditExit.Click += new System.EventHandler(this.btExit_Click);
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label15.Location = new System.Drawing.Point(30, 285);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(183, 25);
+            this.label15.TabIndex = 39;
+            this.label15.Text = "Short Description:";
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label16.Location = new System.Drawing.Point(30, 365);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(180, 25);
+            this.label16.TabIndex = 40;
+            this.label16.Text = "Long Description:";
+            // 
+            // tbEditAddTags
+            // 
+            this.tbEditAddTags.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbEditAddTags.Location = new System.Drawing.Point(269, 76);
+            this.tbEditAddTags.Name = "tbEditAddTags";
+            this.tbEditAddTags.Size = new System.Drawing.Size(206, 31);
+            this.tbEditAddTags.TabIndex = 41;
+            // 
+            // btTagAdd
+            // 
+            this.btTagAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btTagAdd.Location = new System.Drawing.Point(259, 111);
+            this.btTagAdd.Name = "btTagAdd";
+            this.btTagAdd.Size = new System.Drawing.Size(80, 30);
+            this.btTagAdd.TabIndex = 43;
+            this.btTagAdd.Text = "Add Tag";
+            this.btTagAdd.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(792, 23);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(311, 411);
+            this.groupBox1.TabIndex = 44;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // btEditRemove
+            // 
+            this.btEditRemove.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btEditRemove.Location = new System.Drawing.Point(688, 394);
+            this.btEditRemove.Name = "btEditRemove";
+            this.btEditRemove.Size = new System.Drawing.Size(100, 40);
+            this.btEditRemove.TabIndex = 45;
+            this.btEditRemove.Text = "Remove";
+            this.btEditRemove.UseVisualStyleBackColor = true;
+            // 
+            // btEditAdd
+            // 
+            this.btEditAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btEditAdd.Location = new System.Drawing.Point(686, 315);
+            this.btEditAdd.Name = "btEditAdd";
+            this.btEditAdd.Size = new System.Drawing.Size(102, 40);
+            this.btEditAdd.TabIndex = 46;
+            this.btEditAdd.Text = "Add";
+            this.btEditAdd.UseVisualStyleBackColor = true;
+            // 
+            // btEditDeleteTag
+            // 
+            this.btEditDeleteTag.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btEditDeleteTag.Location = new System.Drawing.Point(386, 247);
+            this.btEditDeleteTag.Name = "btEditDeleteTag";
+            this.btEditDeleteTag.Size = new System.Drawing.Size(80, 32);
+            this.btEditDeleteTag.TabIndex = 47;
+            this.btEditDeleteTag.Text = "Delete";
+            this.btEditDeleteTag.UseVisualStyleBackColor = true;
+            // 
+            // btEditAddImage
+            // 
+            this.btEditAddImage.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btEditAddImage.Location = new System.Drawing.Point(106, 174);
+            this.btEditAddImage.Name = "btEditAddImage";
+            this.btEditAddImage.Size = new System.Drawing.Size(85, 67);
+            this.btEditAddImage.TabIndex = 48;
+            this.btEditAddImage.Text = "Add Image";
+            this.btEditAddImage.UseVisualStyleBackColor = true;
+            this.btEditAddImage.Click += new System.EventHandler(this.btEditAddImage_Click);
+            // 
+            // pbRegister
+            // 
+            this.pbRegister.Location = new System.Drawing.Point(12, 12);
+            this.pbRegister.Name = "pbRegister";
+            this.pbRegister.Size = new System.Drawing.Size(179, 156);
+            this.pbRegister.TabIndex = 10;
+            this.pbRegister.TabStop = false;
+            this.pbRegister.Click += new System.EventHandler(this.pbRegister_Click);
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(259, 147);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(207, 94);
+            this.checkedListBox1.TabIndex = 49;
+            // 
+            // fEditPage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(1109, 487);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.btEditAddImage);
+            this.Controls.Add(this.btEditDeleteTag);
+            this.Controls.Add(this.btEditAdd);
+            this.Controls.Add(this.btEditRemove);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btTagAdd);
+            this.Controls.Add(this.tbEditAddTags);
+            this.Controls.Add(this.label16);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.btEditExit);
+            this.Controls.Add(this.btEditClear);
+            this.Controls.Add(this.tbEditLongDesc);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pbRegister);
+            this.Controls.Add(this.btEditSave);
+            this.Controls.Add(this.tbEditShortDesc);
+            this.Controls.Add(this.tbEditTittle);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "fEditPage";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Edit Page";
+            ((System.ComponentModel.ISupportInitialize)(this.pbRegister)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox tbEditTittle;
+        private System.Windows.Forms.TextBox tbEditShortDesc;
+        private System.Windows.Forms.Button btEditSave;
+        private System.Windows.Forms.PictureBox pbRegister;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbEditLongDesc;
+        private System.Windows.Forms.Button btEditClear;
+        private System.Windows.Forms.Button btEditExit;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.TextBox tbEditAddTags;
+        private System.Windows.Forms.Button btTagAdd;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btEditRemove;
+        private System.Windows.Forms.Button btEditAdd;
+        private System.Windows.Forms.Button btEditDeleteTag;
+        private System.Windows.Forms.Button btEditAddImage;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+    }
+}
